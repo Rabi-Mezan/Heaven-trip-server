52b7117520ee9f9832d6ffe10e9d506b981aea02
Rabi-Mezan <ravimezan@gmail.com>
2021-10-29 23:11:50 +0600

    db connected and post api done

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+require("dotenv").config()
+const { MongoClient } = require("mongodb");
+const port = process.env.PORT || 5000;
+
+
+// middlewere
+app.use(cors())
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.krqaw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('HeavenTrip')
+        const packageCollection = database.collection("packages")
+
+
+        app.post('/addpackage', async (req, res) => {
+            const package = req.body;
+            const result = await packageCollection.insertOne(package);
+            res.send(result)
+        })
+
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
